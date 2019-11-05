7f000bab0e0ba7ff016ec1e356e61d2e26185c9c
mif <bad.mifan@gmail.com>
2019-11-05 23:19:34 +0200

    Create hometask2.py
    added 1 part of hometask2

---
+++ hometask2.py
@@ -0,0 +1,10 @@
+# 1
+philosophy = "Beautiful is better than ugly. Explicit is better than implicit. Simple is better than complex. " \
+            "Complex is better than complicated. Flat is better than nested. Sparse is better than dense.Readability counts."
+print("Count of word 'better' - ", philosophy.count('better'))
+print("Count of word 'never' - ", philosophy.count('never'))
+print("Count of word 'is' - ", philosophy.count('is'))
+print(philosophy.upper())
+print(philosophy.replace('i', '&'))
+# 2
+
