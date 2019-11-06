75a42c86dcbf1d04222b90cc5732e4ac48e02319
mif <bad.mifan@gmail.com>
2019-11-06 13:00:54 +0200

    Update hometask2.py
    added 2 and 3 tasks

--- hometask2.py
+++ hometask2.py
@@ -7,4 +7,16 @@ print("Count of word 'is' - ", philosophy.count('is'))
 print(philosophy.upper())
 print(philosophy.replace('i', '&'))
 # 2
-
+n = str(4832)
+product = int(n[0])*int(n[1])*int(n[2])*int(n[3])
+print("Product of multiplication is ", product)
+print("Reverse writing of number is ", int(n[::-1]))
+print("Sorted number is ", int(''.join(sorted(n))))
+# 3
+a = 5
+b = 3
+a = a + b
+b = a - b
+a = a - b
+print("a = ",a)
+print("b = ",b)
